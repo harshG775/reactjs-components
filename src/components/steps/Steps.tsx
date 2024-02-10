e7031c03c307435b9c2203cc1ec82f6d25a0103f
Harsh <hgaur491@gmail.com>
2024-02-10 09:04:05 +0530

    fix for build

--- src/components/steps/Steps.tsx
+++ src/components/steps/Steps.tsx
@@ -8,11 +8,11 @@ const stepsData = [
     { id: "Step 3", step:3, name: "Tell us where you live",},
     { id: "Step 4", step:4, name: "Any of the insured member have a medical history?",},
 ];
-function StepsComponent({stepsData,currentStep,handleStep}) {
+function StepsComponent({stepsData,currentStep,handleStep}:{stepsData:any,currentStep:number,handleStep:any}) {
 	return (
 		<nav aria-label="Progress">
 			<ol className="space-y-4 md:flex md:space-y-0 md:space-x-8">
-            {stepsData.map((step) => (
+            {stepsData.map((step:{id:string,step:number,name:string}) => (
                 <li key={step.name} className="md:flex-1">
                     {step.step <= currentStep ? (
                         <div onClick={()=>handleStep(step.step)} className=" cursor-pointer group pl-4 py-2 flex flex-col border-l-4 border-indigo-600 hover:border-indigo-800 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4" >
@@ -76,7 +76,7 @@ export default function Steps() {
 		}
 		setCurrentStep((prev) => prev + 1);
 	};
-    const handleStep = (step) => {
+    const handleStep = (step:number) => {
         setCurrentStep(step);
     }
 	return (
