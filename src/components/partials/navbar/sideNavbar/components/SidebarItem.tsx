0a640f06a4aebc1efd3a52809e6942d4705ba519
Harsh <hgaur491@gmail.com>
2024-02-17 14:23:15 +0530

    adding React Syntax Highlighter

--- src/components/partials/navbar/sideNavbar/components/SidebarItem.tsx
+++ src/components/partials/navbar/sideNavbar/components/SidebarItem.tsx
@@ -1,7 +1,6 @@
 "use client"
 import Link from "next/link";
 function SidebarSubItem({linkData}:any) {
-    console.log(linkData)
     return (
         <li>
             <Link href={linkData.href}>{linkData.title}</Link>
@@ -9,7 +8,6 @@ function SidebarSubItem({linkData}:any) {
     );
 }
 export default function SidebarItem({linkData}:any) {
-    console.log(linkData)
     if (linkData.haveSubMenu) {
         return (
             <li>
