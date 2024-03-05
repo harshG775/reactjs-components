baa2990f67899abd0e391fd0b6436866d9d5ea33
Harsh <hgaur491@gmail.com>
2024-03-05 18:29:55 +0530

    //

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -1,8 +1,14 @@
+"use client"
+import { useState } from "react";
+
 function Tab({ ...data }) {
     const { name } = data;
-    return <div>{name}</div>;
+    return <div className="p-2">
+        {name}
+    </div>;
 }
 export default function Tabs() {
+    const [activeTab, setActiveTab ] = useState(1);
     const tabData = [
         {
             id: 1,
@@ -16,7 +22,7 @@ export default function Tabs() {
     return (
         <div className=" min-w-screen w-full grid grid-cols-[repeat(auto-fill,minmax(80px,1fr))]">
             {tabData.map((data) => (
-                <Tab key={data.id} {...data} />
+                <Tab key={data.id} {...data} activeTab={activeTab} setActiveTab={setActiveTab} />
             ))}
         </div>
     );
