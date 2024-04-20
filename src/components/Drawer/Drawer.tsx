57c8c2090084d4088d5a3ec489543e7ee2e2bea3
Harsh <hgaur491@gmail.com>
2024-04-20 20:52:10 +0530

    creating Drawer

---
+++ src/components/Drawer/Drawer.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { cn } from "@/utils/cn";
+type SideBarProps = {
+    children: React.ReactNode;
+    isDrawerOpen: boolean;
+    setIsDrawerOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    className?: string;
+    from: "left" | "right" | "top" | "bottom";
+};
+export default function Drawer(props: SideBarProps) {
+    const {
+        children,
+        isDrawerOpen,
+        setIsDrawerOpen,
+        className,
+        from = "left",
+    } = props;
+    const direction = {
+        left: `
+        ${isDrawerOpen ? "-translate-x-0" : "translate-x-full"}
+        top-0 right-0 bottom-0 left-auto
+    `,
+        right: `
+        ${isDrawerOpen ? "translate-x-0" : "-translate-x-full"}
+        top-0 right-auto bottom-0 left-0
+    `,
+        top: `
+        ${isDrawerOpen ? "translate-y-0" : "-translate-y-full"}
+        top-0 right-0 bottom-auto left-0
+    `,
+        bottom: `
+        ${isDrawerOpen ? "translate-y-0" : "translate-y-full"}
+        top-auto right-0 bottom-0 left-0
+    `,
+    };
+
+    return (
+        <>
+            <aside
+                className={`
+                ${cn(
+                    "z-50 fixed transition bg-neutral-900 text-neutral-50",
+                    className
+                )}
+                ${direction[from]}
+            `}
+            >
+                {children}
+            </aside>
+            {isDrawerOpen?
+            <div  className={` fixed inset-0 bg-neutral-950/50 z-40`} onClick={() => setIsDrawerOpen(false)}></div>
+            :""}
+        </>
+    );
+}
+
+
+
+
+
+// function Example() {
+//     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+//     return (
+//         <div>
+//             <button
+//                 type="button"
+//                 onClick={() => setIsDrawerOpen(!isDrawerOpen)}
+//             >
+//                 trigger
+//             </button>
+//             <Drawer
+//                 isDrawerOpen={isDrawerOpen}
+//                 setIsDrawerOpen={setIsDrawerOpen}
+//                 from="bottom"
+//             >
+//                 <div>test data</div>
+//                 <div>test data</div>
+//                 <div>test data</div>
+//                 <div>test data</div>
+//                 <div>test data</div>
+//             </Drawer>
+//         </div>
+//     );
+// }
