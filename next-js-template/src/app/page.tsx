b23bf596a278902f4f180fae2f8aef5d58d983fd
Harsh <hgaur491@gmail.com>
2024-05-27 11:02:23 +0530

    ////

--- next-js-template/src/app/page.tsx
+++ next-js-template/src/app/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className="grid place-content-center gap-4 h-screen text-center">
+        <main className="grid place-content-center gap-2 h-screen text-center">
             <h1 className="text-2xl font-bold text-primary">Template</h1>
             <p className=" text-2xl max-w-96 text-foreground/50">
                 NextJs template with theme <br /> pre-Installed packages
             </p>
-            <div>
+            <div className="text-md">
                 get new theme from <Link 
                     href="https://ui.shadcn.com/themes"
                     target="_blank"
