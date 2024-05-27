d61faff40ffb527008d7a79a9e0f44caaa2debea
Harsh <hgaur491@gmail.com>
2024-05-27 11:22:36 +0530

    //

--- next-js-template/src/app/page.tsx
+++ next-js-template/src/app/page.tsx
@@ -4,16 +4,30 @@ export default function Home() {
     return (
         <main className="grid place-content-center gap-2 h-screen text-center">
             <h1 className="text-2xl font-bold text-primary">Template</h1>
-            <p className=" text-2xl max-w-96 text-foreground/50">
-                NextJs template with theme <br /> pre-Installed packages
-            </p>
             <div className="text-md">
-                get new theme from <Link 
+                get new theme from{" "}
+                <Link
                     href="https://ui.shadcn.com/themes"
                     target="_blank"
-                    className="hover:underline text-primary font-medium">Shadcn/theme
-                    </Link>
+                    className="hover:underline text-primary font-medium"
+                >
+                    Shadcn/theme
+                </Link>
+                <br />
+                or
+                <br />
+                <Link
+                    href="https://zippystarter.com/tools/shadcn-ui-theme-generator"
+                    target="_blank"
+                    className="hover:underline text-primary font-medium"
+                >
+                    zippystarter{" "}
+                </Link>
+                for more customization
             </div>
+            <p className=" text-xl max-w-96 text-foreground/50">
+                NextJs template with theme <br /> pre-Installed packages
+            </p>
         </main>
     );
 }
