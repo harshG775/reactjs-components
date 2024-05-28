66c2445f402b69210544fc3fa1e2f030255b0387
Harsh <hgaur491@gmail.com>
2024-05-28 17:35:53 +0530

    drawer and button

---
+++ web/src/components/ui/Drawer/Drawer.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
+type SideBarProps = {
+    children: React.ReactNode;
+    isDrawerOpen: boolean;
+    setIsDrawerOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    className?: string;
+    from: "left" | "right" | "top" | "bottom" | "bottomLeft" | "bottomRight" | "topLeft" | "topRight";
+};
+export default function Drawer(rest: SideBarProps) {
+    const {
+        children,
+        isDrawerOpen,
+        setIsDrawerOpen,
+        className,
+        from, 
+        ...props
+    } = rest;
+    const drawerVariants = cva(
+        "z-50 fixed transition bg-background text-foreground border border-neutral-50/20", 
+        {
+            variants: {
+                from: {
+                    left: `
+                        ${isDrawerOpen ? "translate-x-0" : "-translate-x-full"}
+                        top-0 right-auto bottom-0 left-0
+                    `,
+                    right: `
+                        ${isDrawerOpen ? "-translate-x-0" : "translate-x-full"}
+                        top-0 right-0 bottom-0 left-auto
+                    `,
+                    top: `
+                        ${isDrawerOpen ? "translate-y-0" : "-translate-y-full"}
+                        top-0 right-0 bottom-auto left-0
+                    `,
+                    bottom: `
+                        ${isDrawerOpen ? "translate-y-0" : "translate-y-full"}
+                        top-auto right-0 bottom-0 left-0
+                    `,
+                    bottomLeft: `
+                        ${isDrawerOpen ? "translate-y-0 translate-x-0" : "translate-y-full -translate-x-full"}
+                        top-auto bottom-0 left-0  right-auto
+                    `,
+                    bottomRight: `
+                        ${isDrawerOpen ? "translate-y-0 translate-x-0" : "translate-y-full translate-x-full"}
+                        top-auto bottom-0 left-auto  right-0
+                    `,
+                    topLeft: `
+                        ${isDrawerOpen ? "translate-y-0 translate-x-0" : "-translate-y-full -translate-x-full"}
+                        top-0 bottom-auto left-0  right-auto
+                    `,
+                    topRight: `
+                        ${isDrawerOpen ? "translate-y-0 translate-x-0" : "-translate-y-full translate-x-full"}
+                        top-0 bottom-auto left-auto  right-0
+                    `,
+                },
+            },
+            defaultVariants: {
+                from: "bottom",
+            },
+        }
+    );
+    return (
+        <>
+            <div
+                className={cn(drawerVariants({ from }), className)}
+                {...props}
+            >
+                {children}
+            </div>
+            {isDrawerOpen?
+            <div  className={` fixed inset-0 bg-neutral-950/50 z-40`} onClick={() => setIsDrawerOpen(false)}></div>
+            :""}
+        </>
+    );
+}
+
+
