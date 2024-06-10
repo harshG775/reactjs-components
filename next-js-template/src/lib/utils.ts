dee9fa6df157bdd9cf0124871fd4e4492263cd77
Harsh <hgaur491@gmail.com>
2024-06-10 14:23:12 +0530

    adding formatter

--- next-js-template/src/lib/utils.ts
+++ next-js-template/src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+/*formatters*/
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
+    currency: "USD",
+    style: "currency",
+    maximumFractionDigits: 0,
+});
+export function formatCurrency(number: number) {
+    return CURRENCY_FORMATTER.format(number);
+}
+const NUMBER_FORMATTER = new Intl.NumberFormat("en-US");
+export function formatNumber(number: number) {
+    return NUMBER_FORMATTER.format(number);
+}
